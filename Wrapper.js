a61bcca7de4b5605a5d2207141be211eb3636092
atxrenegade <harleighabel@gmail.com>
2021-01-14 23:32:34 -0600

    add wrap function

--- Wrapper.js
+++ Wrapper.js
@@ -1,5 +1,30 @@
 class Wrapper {
+  static wrap(userStr, colNum) {
+    var userStrArr = userStr.split(" ");
+    var newStr = "";
+    var colCount = 0;
 
+    for (let i = 0; i < userStrArr.length; i++) {
+      let word = userStrArr[i];
+      let wordLen = userStrArr[i].length;
+      // if word fits in column at current index without exceeding limit 
+      // add word and update column index count    
+      if ((wordLen + colCount) < colNum) {
+        newStr += `${word}`;
+        colCount += (wordLen + 1)
+        // if word exceeds column limit, break word on final index
+        // add remaining word portion to new column, update column index count
+      } else if (wordLen > (colNum)) {
+        newStr += `\n${word.substring(0, colNum - 1)}-\n${word.substring(colNum - 1)} `;
+        colCount = word.substring(colNum - 1).length;
+        // else add line break then word, update column index count   
+      } else {
+        newStr += `\n${word} `;
+        colCount = wordLen;
+      }
+    }
+    console.log(newStr);
+  }
 }
 
 module.exports = Wrapper;
